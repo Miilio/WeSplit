ea3f5c61ca51e53720fd23d2a42b79a845addd99
Emilio Higueras <emhimu@gmail.com>
2024-06-18 20:03:25 +0200

    Uso de Form

--- WeSplit/ContentView.swift
+++ WeSplit/ContentView.swift
@@ -9,7 +9,12 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-       Text("Hello World")
+        Form {
+            Text("Hello World")
+            Text("Hello World 1")
+            Text("Hello World 2")
+            Text("Hello World 3")
+        }
     }
 }
 
