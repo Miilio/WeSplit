7239cac968d9e21993198549ce2fa6436b5add51
Emilio Higueras <emhimu@gmail.com>
2024-06-18 19:47:28 +0200

    Initial Commit

---
+++ WeSplit/WeSplitApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeSplitApp.swift
+//  WeSplit
+//
+//  Created by Emilio Higueras Muñoz on 18/6/24.
+//
+
+import SwiftUI
+
+@main
+struct WeSplitApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
